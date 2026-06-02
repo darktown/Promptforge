a64da409b4a40646d18f736232cf7933be0c66ee
darktown <tiefflieger1981@googlemail.com>
2026-06-02 18:42:47 +0200

    feat: add database skeleton

---
+++ backend/database/database.py
@@ -0,0 +1,16 @@
+"""
+Database Connection Layer
+
+Future responsibility:
+- PostgreSQL connection
+- CRUD operations
+- Prompt history
+- User projects
+"""
+
+
+class Database:
+
+    def connect(self):
+        # TODO: PostgreSQL connection.
+        pass
