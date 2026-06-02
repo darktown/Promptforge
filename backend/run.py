f88be4a543249a60ad5a1a35fae3c11089f47f25
darktown <tiefflieger1981@googlemail.com>
2026-06-02 19:33:20 +0200

    feat: add local startup script

---
+++ backend/run.py
@@ -0,0 +1,17 @@
+"""
+Local Development Startup
+
+Run:
+python run.py
+"""
+
+import uvicorn
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+        reload=True
+    )
